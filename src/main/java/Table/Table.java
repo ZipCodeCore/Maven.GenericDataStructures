81c8c337529fb508410b4d31b0acb432124eefd3
Mark A Brown <markabrown87@gmail.com>
2017-05-17 17:10:41 -0400

    Finished Part 3

--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -1,6 +1,7 @@
 package Table;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * This class needs to manage an ArrayList of Entry objects.  It needs a get method that takes a key and returns
@@ -10,8 +11,54 @@ import java.util.ArrayList;
  * Void return on `remove`.
  */
 public class Table<K, V> {
-    private ArrayList entries;
 
-    public Table() {
+    private ArrayList<Entry<K,V>> entries;
+
+    public Table() {entries = new ArrayList<Entry<K,V>>();}
+
+    public V get(K key) {
+        for (int index = 0; index < entries.size(); index++) {
+            if (entries.get(index).getKey().equals(key)) {
+                return entries.get(index).getValue();
+            }
+        }
+        return null;
     }
+
+    public int getIndexOfEntryKey (K key) {
+        for (int index = 0; index < entries.size(); index++) {
+            if (entries.get(index).getKey().equals(key)) {
+                return index;
+            }
+        }
+        return entries.size();
+    }
+
+    public void put(K key, V value) {
+        if (getIndexOfEntryKey(key) == entries.size()) {
+            addNewEntry(key,value);
+        } else {
+            replaceExistingEntry(key, value, getIndexOfEntryKey(key));
+        }
+    }
+
+    public void addNewEntry(K key, V value) {
+        Entry<K,V> newEntry = new Entry<K, V>(key, value);
+        entries.add(newEntry);
+    }
+
+    public void replaceExistingEntry(K key, V value, int index) {
+        Entry<K,V> newEntry = new Entry<K, V>(key, value);
+        entries.add(index, newEntry);
+        entries.remove(index + 1);
+    }
+
+    public void remove (K key) {
+        for (int index = 0; index < entries.size(); index++) {
+            if (entries.get(index).getKey().equals(key)) {
+                entries.remove(index);
+            }
+        }
+    }
+
 }
