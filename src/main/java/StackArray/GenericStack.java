6753cc7483d335ed5a74626468d7c564db9f0774
wongandrewp <wong.andrew.p@gmail.com>
2017-05-17 13:25:15 -0400

    Generic Stack passing all tests, taking care of warnings now

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,8 +1,5 @@
 package StackArray;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
  * Remember, you might need to resize the stack in the push method.
@@ -12,7 +9,7 @@ public class GenericStack<E> {
     private E[] elements;
 
     public GenericStack() {
-        elements = new E[32];
+        elements = (E[]) new Object[0];
     }
 
     public boolean isEmpty(){
@@ -21,6 +18,29 @@ public class GenericStack<E> {
     }
 
     public void push(E e){
-        elements[elements.length] = e;
+        expandElementsByOne();
+        elements[elements.length-1] = e;
+    }
+
+    private void expandElementsByOne(){
+        E[] elementsClone = elements.clone();
+        elements = (E[]) new Object[elements.length+1];
+        for(int i = 0; i < elementsClone.length; i++){
+            elements[i] = elementsClone[i];
+        }
+    }
+
+    public E pop(){
+        E e = elements[elements.length -1];
+        removeLastElement();
+        return e;
+    }
+
+    private void removeLastElement(){
+        E[] elementsClone = elements.clone();
+        elements = (E[]) new Object[elements.length-1];
+        for(int i = 0; i < elements.length; i++){
+            elements[i] = elementsClone[i];
+        }
     }
 }
--- src/test/java/StackArray/GenericStackTest.java
+++ src/test/java/StackArray/GenericStackTest.java
@@ -16,26 +16,26 @@ public class GenericStackTest {
         // Then it shouldn't be empty
         Assert.assertEquals(false, stack.isEmpty());
     }
-//
-//    @Test
-//    public void testPushingAndPoppingOrder() throws Exception {
-//        // Given an empty stack
-//        GenericStack<String> stack = new GenericStack<>();
-//        // When we push two elements on it
-//        stack.push("foo");
-//        stack.push("bar");
-//        // Then we should see them returned in the correct order
-//        Assert.assertEquals("bar", stack.pop());
-//        Assert.assertEquals("foo", stack.pop());
-//    }
-//
-//    @Test(expected = IndexOutOfBoundsException.class)
-//    public void testPopFirst() throws Exception {
-//        // Given an empty stack
-//        GenericStack<String> stack = new GenericStack<>();
-//        // When it's popped
-//        stack.pop();
-//        // Then we should get an exception
-//    }
+
+    @Test
+    public void testPushingAndPoppingOrder() throws Exception {
+        // Given an empty stack
+        GenericStack<String> stack = new GenericStack<>();
+        // When we push two elements on it
+        stack.push("foo");
+        stack.push("bar");
+        // Then we should see them returned in the correct order
+        Assert.assertEquals("bar", stack.pop());
+        Assert.assertEquals("foo", stack.pop());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testPopFirst() throws Exception {
+        // Given an empty stack
+        GenericStack<String> stack = new GenericStack<>();
+        // When it's popped
+        stack.pop();
+        // Then we should get an exception
+    }
 
 }
