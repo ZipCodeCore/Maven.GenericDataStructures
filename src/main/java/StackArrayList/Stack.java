afaa93aefde2dceff9e999cc6e97d4c50c20dc6c
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-17 14:54:37 -0400

    Finished section1

--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -1,16 +1,45 @@
 package StackArrayList;
 
+
 import java.util.ArrayList;
 
 /**
  * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
-public class Stack<E> {
-    private ArrayList elements;
+public class Stack<E> extends ArrayList {
+
+    private ArrayList<E> stack = new ArrayList<>();
 
 
     public Stack(){
+        ArrayList<E> stack = new ArrayList<>();
+    }
+
+    public E push(E e){
+        stack.add(e);
+        return e;
+    }
+
+    public boolean isEmpty(){
+        if (stack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public E pop()  {
+
+        if (!isEmpty()) {
+            E value = (E) stack.remove(stack.size() - 1);
+            return value;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+
+
 
     }
+
 }
