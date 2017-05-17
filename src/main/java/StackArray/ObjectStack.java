d5a7e9f63cc9bdd7245c17013145e99d4a0b2d33
Molly Arant <mccormick.mollyt@gmail.com>
2017-05-17 19:24:17 -0400

    completed part two of the microlab

--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -11,6 +11,30 @@ public class ObjectStack<E> {
     private Object[] elements;
 
     public ObjectStack() {
+        elements= new Object[0];
 
     }
+
+    public void push(Object item) {
+        elements = Arrays.copyOf(elements, elements.length+1);
+        elements[elements.length-1]=item;
+        for(Object e : elements){
+        }
+
+    }
+
+    public Object pop() throws IndexOutOfBoundsException{
+        Object lastElement= elements[elements.length-1];
+
+        if (elements.length==0) {
+            throw new IndexOutOfBoundsException();
+        }
+        elements=Arrays.copyOf(elements, elements.length-1);
+        return lastElement;
+    }
+
+    public boolean isEmpty(){
+        return elements.length==0;
+    }
+
 }
