d894ae393ef8a723365fbd80befb713a29b9e124
cameronsima <cjsima@gmail.com>
2017-05-17 16:20:22 -0400

    Table completed

--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -9,19 +9,47 @@ import java.util.ArrayList;
  * It also needs a remove method which takes a key and, if that key exists in the arraylist, removes that item.
  * Void return on `remove`.
  */
-//public class Table<K, V> {
-//    private ArrayList entries;
-//
-//    public Entry get(K key) {
-//
-//        for (Entry entry : entries) {
-//            if (entry.getKey().equals(key)) {
-//                return entry;
-//            }
-//        }
-//    }
-//
-//    public Table() {
-//        entries = new ArrayList();
-//    }
-//}
+public class Table<K, V> {
+    private ArrayList<Entry> entries;
+
+    public Entry getEntry(K key) {
+
+        for (Entry entry : entries) {
+            if (entry.getKey().equals(key)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    public V get(K key) {
+        try {
+            return (V)getEntry(key).getValue();
+        } catch (NullPointerException e) {
+            return null;
+        }
+
+    }
+
+    public void put(K key, V value) {
+        Entry newEntry = new Entry(key, value);
+        Entry existingEntry = getEntry(key);
+
+        if (existingEntry == null) {
+            entries.add(newEntry);
+        } else {
+            entries.set(entries.indexOf(existingEntry), newEntry);
+        }
+
+
+    }
+
+    public void remove(K key) {
+
+        entries.remove(getEntry(key));
+    }
+
+    public Table() {
+        entries = new <Entry>ArrayList();
+    }
+}
