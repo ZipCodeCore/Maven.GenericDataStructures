54bf84c1d4fe3e8745d8ba8de4dc6e6ca31e5d0c
Aaron Long <aaron.long.c@gmail.com>
2017-05-17 15:54:17 -0400

    Passed all Object Test

--- src/main/java/io/github/aaronclong/generics/stackarray/ObjectStack.java
+++ src/main/java/io/github/aaronclong/generics/stackarray/ObjectStack.java
@@ -1,5 +1,7 @@
 package io.github.aaronclong.generics.stackarray;
 
+import java.util.Arrays;
+
 /**
  * Expand the ArrayList implementation of stack here to use an Object[] array.
  * Still implement push, pop, and isEmpty.
@@ -11,12 +13,38 @@ public class ObjectStack<E> {
   private int currentElement;
 
   public ObjectStack() {
-    elements = 
+    elements = new Object[20];
+  }
+
+  private void resizeArray() {
+    elements = Arrays.copyOf(elements, elements.length * 2);
   }
 
   public boolean isEmpty() {
     return currentElement == 0;
   }
 
+  private void incrementCurrentElement() {
+    currentElement++;
+  }
+
+  private void decrementCurrentElement() {
+    currentElement--;
+  }
+
+  @SuppressWarnings("unchecked")
+  public E pop() {
+    Object objectBuffer = elements[currentElement - 1];
+    decrementCurrentElement();
+    return (E) objectBuffer;
+  }
+
+  public void push(E element) {
+    if (currentElement >= elements.length - 5) {
+      resizeArray();
+    }
+    elements[currentElement] = element;
+    incrementCurrentElement();
+  }
 
 }
