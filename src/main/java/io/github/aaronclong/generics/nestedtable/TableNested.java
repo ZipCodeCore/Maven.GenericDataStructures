b1c5b8f7f6166c79dcfdfe8ce1f927463e7b857d
Aaron Long <aaron.long.c@gmail.com>
2017-05-17 16:13:32 -0400

    Fixed more package stuff

---
+++ src/main/java/io/github/aaronclong/generics/nestedtable/TableNested.java
@@ -0,0 +1,12 @@
+package io.github.aaronclong.generics.nestedtable;
+
+import java.util.ArrayList;
+
+/**
+ * All you need to do for this microlab is take the Table and Entry
+ *  from the last one and make Entry a nested class.
+ * Think about how nested classes should work with generics.
+ */
+public class TableNested<K, V> {
+
+}
