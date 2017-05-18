baf979119d89a18fa7c4066b2b2c597ee0dc736a
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-18 18:37:05 -0400

    MapFunc complete

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -8,5 +8,13 @@ import java.util.function.Function;
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
 public class MapFunc {
-
+    public static <T, R> ArrayList map(ArrayList<T> arrayList , Function<T,R> function){
+       ArrayList<T> toReturn = new ArrayList<T>();
+       for(int i = 0; i < arrayList.size(); i++){
+           T temp = arrayList.get(i);
+           temp = (T) function.apply(temp);
+           toReturn.add(temp);
+       }
+       return toReturn;
+    }
 }
