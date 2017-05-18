16ab5c2ea7c1f910f3dffb4c5f76fe1a7ae797d0
Aaron Long <aaron.long.c@gmail.com>
2017-05-18 15:55:07 -0400

    Did more formatting

--- src/main/java/io/github/aaronclong/generics/pair/Arrays.java
+++ src/main/java/io/github/aaronclong/generics/pair/Arrays.java
@@ -3,13 +3,15 @@ package io.github.aaronclong.generics.pair;
 import java.util.ArrayList;
 
 /**
- * In here you must make firstLast, which will return a pair of the first element in the array list and the last
- * element in the arraylist.
+ * In here you must make firstLast, which will return a pair
+ * of the first element in the array list and the last element in the arraylist.
  * You must also make a min method that returns the smallest item in the array list
  * A max method that returns the largest item in the arraylist
- * And a minmax method that returns a pair containing the largest and smallest items from the array list
+ * And a minmax method that returns a pair
+ * containing the largest and smallest items from the array list
  */
 public class Arrays {
-    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-    }
+  public static <E> Pair<E> firstLast(ArrayList<E> a) {
+    return new Pair<E>();
+  }
 }
--- src/main/java/io/github/aaronclong/generics/pair/Pair.java
+++ src/main/java/io/github/aaronclong/generics/pair/Pair.java
@@ -1,12 +1,21 @@
 package io.github.aaronclong.generics.pair;
 
 /**
- * You need to store two values of type `E`, set them in a constructor, and have the following methods,
+ * You need to store two values of type `E`, set them in a constructor,
+ * and have the following methods,
  * getFirst
- * getSecond
+ * getSecond.
  * min -> returns the minimum of the pair
  * max -> returns the maximum of the pair
  */
-public class Pair {
+public class Pair<E> {
+  private E first;
+  private E second;
 
+  Pair() {}
+
+  Pair(E first, E second) {
+    this.first = first;
+    this.second = second;
+  }
 }
