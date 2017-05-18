6c094103b693ed78eadd769b70700c3a26cb9e97
cameronsima <cjsima@gmail.com>
2017-05-18 15:31:18 -0400

    working on final lab

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -7,6 +7,16 @@ import java.util.function.Function;
  * Create a function called `map` that takes an ArrayList and a `Function<T,R>` object,
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
-public class MapFunc {
+public  class  MapFunc {
+
+    public static <T,R> ArrayList map(ArrayList<T> list, Function<T,R> obj) {
+        ArrayList<R> result = new ArrayList<>();
+
+        for (T el : list) {
+            result.add(obj.apply(el));
+        }
+
+        return result;
+    }
 
 }
