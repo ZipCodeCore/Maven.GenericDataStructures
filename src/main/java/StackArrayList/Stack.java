0d508455ec89bd4e103770d3c62e403987e712d1
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-05-18 18:06:07 -0400

    Part 1

--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -11,6 +11,19 @@ public class Stack<E> {
 
 
     public Stack(){
-
+        elements = new ArrayList<E>();
     }
+
+    public boolean push(E item){
+        return elements.add(item);
+    }
+
+    public E pop(){
+        return (E) elements.remove(elements.size()-1);
+    }
+
+    public boolean isEmpty(){
+        return elements.isEmpty();
+    }
+
 }
