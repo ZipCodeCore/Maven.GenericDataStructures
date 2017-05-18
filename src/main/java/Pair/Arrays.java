1b2e975947b8d9a4f032a35cd01a998b351aa6f0
Justin Haas <justinmhaas1@gmail.com>
2017-05-18 17:20:58 -0400

    edited pair class

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -16,17 +16,25 @@ public class Arrays {
     }
 
     public static <E extends Comparable> E min(ArrayList<E> list){
-        Collections.sort(list);
-        return list.get(0);
+        E min = list.get(0);
+        for (E item : list){
+            if(item.compareTo(min)<0)
+                min = item;
+        }
+
+        return min;
     }
 
     public static <E extends Comparable> E max(ArrayList<E> list){
-        Collections.sort(list);
-        return list.get(list.size()-1);
+        E max = list.get(0);
+        for (E item : list){
+            if(item.compareTo(max)>0)
+                max = item;
+        }
+        return max;
     }
 
     public static <E extends Comparable> Pair<E> minMax(ArrayList<E> a){
-        Collections.sort(a);
         return new Pair<E>(a.get(0), a.get(a.size()-1));
 
     }
