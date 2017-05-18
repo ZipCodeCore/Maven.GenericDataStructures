248bd4f98d9d96fa585d4e55b380d73e115a798f
Aaron Long <aaron.long.c@gmail.com>
2017-05-18 14:09:44 -0400

    Updated stuff

--- src/main/java/io/github/aaronclong/generics/arraylistcombiner/ArrayListCombiner.java
+++ src/main/java/io/github/aaronclong/generics/arraylistcombiner/ArrayListCombiner.java
@@ -1,13 +1,20 @@
 package io.github.aaronclong.generics.arraylistcombiner;
 
+import io.github.aaronclong.generics.employee.Manager;
+import io.github.aaronclong.generics.employee.Employee;
+
 import java.util.ArrayList;
 
 /**
- * Create two generic methods that take two arraylists.
+ * Create two generic methods that take two ArrayLists.
  * The methods should both append the second ArrayList's items,
  * to the first.  Use a wildcard for one of the type arguments in each method.
  * The first method should be called extendCombiner and should use ? extends E
  * The second method should be called superCombiner and should use ? super E
  */
 public class ArrayListCombiner {
+  public <E> void superCombiner(ArrayList<? super E> listOne,
+                                ArrayList<? super E> listTwo) {
+
+  }
 }
