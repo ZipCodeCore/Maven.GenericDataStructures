3bf5ba803a7e173f5cd4d5e934cbed2931b70050
Matt Wu <mingzhi.wu12@gmail.com>
2017-05-19 19:55:40 -0400

    done all parts

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -2,6 +2,7 @@ package Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * In here you must make firstLast, which will return a pair of the first element in the array list and the last
@@ -10,7 +11,28 @@ import java.util.Collections;
  * A max method that returns the largest item in the arraylist
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+
+public class Arrays {
+
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+
+        return new Pair<E> (a.get(0), a.get(a.size()-1) );
+    }
+
+    public static <E extends Comparable> E min(ArrayList<E> a){
+
+        Collections.sort(a);
+        return a.get(0);
+    }
+
+    public static <E extends Comparable> E max(ArrayList<E> a){
+
+        Collections.sort(a);
+        return a.get(a.size()-1);
+    }
+
+    public static <E extends Comparable> Pair<E> minMax(ArrayList<E> a){
+
+        return new Pair<E> (min(a), max(a));
+    }
+}
