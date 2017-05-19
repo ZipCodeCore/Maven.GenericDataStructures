f54f05445738f4e11b7b284c7eb2004c4406df01
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-05-19 15:24:21 -0400

    map

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -9,4 +9,14 @@ import java.util.function.Function;
  */
 public class MapFunc {
 
+    public static <T, R> ArrayList<R> map(ArrayList<T> arraylistT, Function<T,R> function){
+
+        ArrayList<R> arraylistR = new ArrayList<>();
+
+        for(int i =0 ; i < arraylistT.size(); i++){
+            arraylistR.add(function.apply(arraylistT.get(i)));
+        }
+        return arraylistR;
+    }
+
 }
