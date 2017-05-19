5f826af71ccc665fee6dd85cb63238ab30f58d35
Matt Wu <mingzhi.wu12@gmail.com>
2017-05-19 16:28:23 -0400

    Swap done

--- src/main/java/ArrayListCombiner/ArrayListCombiner.java
+++ src/main/java/ArrayListCombiner/ArrayListCombiner.java
@@ -9,4 +9,16 @@ import java.util.ArrayList;
  * The second method should be called superCombiner and should use ? super E
  */
 public class ArrayListCombiner {
+
+    //in test, because arr2 is a childclass of arr1, so  ?extendsE make it so called "inherit" from arr1, so the value can be combined
+    public static <E> void extendCombiner(ArrayList<E> arr1, ArrayList<? extends E> arr2){
+
+        arr1.addAll(arr2);
+    }
+
+    // ?superE make arr1 the super class of arr2
+    public static <E> void superCombiner(ArrayList<? super E> arr1, ArrayList<E> arr2){
+
+        arr1.addAll(arr2);
+    }
 }
