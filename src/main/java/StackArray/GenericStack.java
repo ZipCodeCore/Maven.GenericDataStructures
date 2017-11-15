0f9c42f254a4b4f23843b5687f5ee13149378924
andrew <nehctuk@gmail.com>
2017-11-14 19:50:04 -0500

    Saving progress

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,6 +1,6 @@
 package StackArray;
 
-import java.util.Arrays;
+import StackArrayList.Stack;
 
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
@@ -9,7 +9,31 @@ import java.util.Arrays;
  */
 public class GenericStack<E> {
     private E[] elements;
+    private Stack<E> aStack;
 
     public GenericStack() {
+        aStack = new Stack<>();
+        elements = (E[]) aStack.getElements().toArray();
+    }
+
+    public void push(E element) {
+        aStack.push(element);
+        elements = (E[]) aStack.getElements().toArray();
+    }
+
+    public E pop() {
+        int lastIndex = aStack.getElements().size()-1;
+        E lastElement = aStack.getElements().get(lastIndex);
+        aStack.getElements().remove(lastIndex);
+        return lastElement;
+    }
+
+    public boolean isEmpty() {
+        if (elements.length == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
