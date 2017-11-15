1c4fb80744ca874879efe461b50c87a9c3e3649a
andrew <nehctuk@gmail.com>
2017-11-15 11:08:50 -0500

    Finished lab.

--- src/main/java/Pair/Pair.java
+++ src/main/java/Pair/Pair.java
@@ -7,6 +7,47 @@ package Pair;
  * min -> returns the minimum of the pair
  * max -> returns the maximum of the pair
  */
-public class Pair {
+public class Pair<E extends Comparable> {
+    private E first;
+    private E second;
+
+    public Pair(E first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public E min() {
+        int comparisonValue = first.compareTo(second);
+        if (comparisonValue < 0) {
+            return first;
+        }
+        else if (comparisonValue > 0) {
+            return second;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public E max() {
+        int comparisonValue = first.compareTo(second);
+        if (comparisonValue > 0) {
+            return first;
+        }
+        else if (comparisonValue < 0) {
+            return second;
+        }
+        else {
+            return null;
+        }
+    }
 
 }
