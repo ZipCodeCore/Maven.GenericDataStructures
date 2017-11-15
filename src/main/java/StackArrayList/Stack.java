793c453a738c659645399dd6b399651b616817d8
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-15 08:23:00 -0500

    Add explicit thrown exception

--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -1,37 +1,41 @@
-//package StackArrayList;
-//
-//import java.util.ArrayList;
-//
-///**
-// * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
-// * If you pop on an empty stack, throw an IndexOutOfBoundsException.
-// */
-//public class Stack<E> {
-//    private ArrayList<E> elements;
-//
-//    public Stack(){
-//        elements = new ArrayList<>();
-//    }
-//
-//    public E pop()
-//    {
-//        E temp = elements.get(elements.size()-1);
-//
-//        elements.remove(elements.size()-1);
-//
-//        return (temp);
-//
-//    }
-//
-//    public void push(E element)
-//    {
-//        elements.add(element);
-//    }
-//
-//    public boolean isEmpty()
-//    {
-//        return (elements.isEmpty());
-//    }
-//
-//
-//}
+package StackArrayList;
+
+import java.util.ArrayList;
+
+/**
+ * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
+ * If you pop on an empty stack, throw an IndexOutOfBoundsException.
+ */
+public class Stack<E> {
+    private ArrayList<E> elements;
+
+    public Stack(){
+        elements = new ArrayList<>();
+    }
+
+    public E pop() throws IndexOutOfBoundsException
+    {
+        try {
+            E temp = elements.get(elements.size() - 1);
+
+            elements.remove(elements.size() - 1);
+
+            return (temp);
+        }catch (IndexOutOfBoundsException ioobe){
+            throw ioobe;
+        }
+
+    }
+
+    public void push(E element)
+    {
+        elements.add(element);
+    }
+
+    public boolean isEmpty()
+    {
+        return (elements.isEmpty());
+    }
+
+
+}
