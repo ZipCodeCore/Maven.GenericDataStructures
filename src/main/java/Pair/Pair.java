584e2565d4f37b878557d0338dae93bea3ec7b48
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-15 11:08:31 -0500

    all done

--- src/main/java/Pair/Pair.java
+++ src/main/java/Pair/Pair.java
@@ -1,5 +1,7 @@
 package Pair;
 
+import java.util.Comparator;
+
 /**
  * You need to store two values of type `E`, set them in a constructor, and have the following methods,
  * getFirst
@@ -7,6 +9,45 @@ package Pair;
  * min -> returns the minimum of the pair
  * max -> returns the maximum of the pair
  */
-public class Pair {
+public class Pair<E extends Comparable> {
+
+    private E first;
+    private E second;
+
+    public Pair(E first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E min() {
+        int comparisionValue = first.compareTo(second);
+        if (comparisionValue < 0) {
+            return first;
+        }
+        else if (comparisionValue > 0) {
+            return second;
+        }
+            return null;
+    }
+
+    public E max() {
+        int comparisionValue = first.compareTo(second);
+        if (comparisionValue > 0) {
+            return first;
+        }
+        else if (comparisionValue < 0) {
+            return second;
+        }
+        return null;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
 
 }
