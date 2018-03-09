d3b573b784561b9dabbfe1c4a274de94e3dfb1dc
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-09 11:22:40 -0500

    generics...sigh

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -1,16 +1,30 @@
-//package Pair;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//
-///**
-// * In here you must make firstLast, which will return a pair of the first element in the array list and the last
-// * element in the arraylist.
-// * You must also make a min method that returns the smallest item in the array list
-// * A max method that returns the largest item in the arraylist
-// * And a minmax method that returns a pair containing the largest and smallest items from the array list
-// */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+package Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * In here you must make firstLast, which will return a pair of the first element in the array list and the last
+ * element in the arraylist.
+ * You must also make a min method that returns the smallest item in the array list
+ * A max method that returns the largest item in the arraylist
+ * And a minmax method that returns a pair containing the largest and smallest items from the array list
+ */
+public class Arrays {
+
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        return new Pair<>(a.get(0), a.get(a.size() - 1));
+    }
+
+    public static <E extends Comparable> Comparable min(ArrayList<E> a) {
+        return Collections.min(a);
+    }
+
+    public static <E extends Comparable> Comparable max(ArrayList<E> a) {
+        return Collections.max(a);
+    }
+
+    public static <E extends Comparable> Pair minMax(ArrayList<E> a) {
+        return new Pair<>(min(a), max(a));
+    }
+}
--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -1,22 +1,47 @@
-//package Table;
-//
-//import java.util.ArrayList;
-//
-///**
-// * This class needs to manage an ArrayList of Entry objects.  It needs a get method that takes a key and returns
-// * its corresponding value, or null of the key is not in the arraylist.  It needs a put method that takes a key and value
-// * and makes an entry with key, value.  NOTE: There cannot be two entries with the same key.
-// * It also needs a remove method which takes a key and, if that key exists in the arraylist, removes that item.
-// * Void return on `remove`.
-// */
-//public class Table <K, V> {
-//    private ArrayList<Entry> entries;
-//
-//    public Table() {
-//        entries = new ArrayList();
-//    }
-//
-//    public Entry<K, V> get(K valueToGet) {
-//        return entries.get(valueToGet, );
-//    }
-//}
+package Table;
+
+import java.util.ArrayList;
+
+/**
+ * This class needs to manage an ArrayList of Entry objects.  It needs a get method that takes a key and returns
+ * its corresponding value, or null of the key is not in the arraylist.  It needs a put method that takes a key and value
+ * and makes an entry with key, value.  NOTE: There cannot be two entries with the same key.
+ * It also needs a remove method which takes a key and, if that key exists in the arraylist, removes that item.
+ * Void return on `remove`.
+ */
+public class Table <K, V> {
+    private ArrayList<Entry> entries;
+
+    public Table() {
+        entries = new ArrayList();
+    }
+
+    public void put(K key, V value) {
+        int i = 0;
+        for(Entry entry: entries) {
+            if(entry.getKey().equals(key)) {
+                entries.set(i, new Entry<>(key, value));
+                return;
+            }
+                i++;
+        }
+        entries.add(new Entry<>(key, value));
+    }
+
+    public Object get(K key) {
+        for(Entry entry : entries) {
+            if(entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        for(int i = 0; i < entries.size(); i++) {
+            if(entries.get(i).getKey().equals(key)) {
+                entries.remove(i);
+            }
+        }
+    }
+}
