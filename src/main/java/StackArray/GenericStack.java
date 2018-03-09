4fadc01d9cb78d73e382c5ce0374f797ad7d0b94
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-09 18:03:30 -0500

    saving

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -11,5 +11,24 @@ public class GenericStack<E> {
     private E[] elements;
 
     public GenericStack() {
+        elements = (E[]) new Object[0];
     }
+
+    public void push(E element){
+        elements = Arrays.copyOf(elements,elements.length+1);
+        elements[elements.length-1] = element;
+
+    }
+
+    public E pop(){
+        E element = elements[elements.length-1];
+        elements = Arrays.copyOf(elements,elements.length-1);
+        return element;
+    }
+
+    public boolean isEmpty(){
+        return elements.length == 0;
+    }
+
+
 }
--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -11,6 +11,24 @@ public class ObjectStack<E> {
     private Object[] elements;
 
     public ObjectStack() {
+        elements = new Object[0];
 
     }
+
+    public void push(Object element){
+        elements = Arrays.copyOf(elements,elements.length+1);
+        elements[elements.length-1] = element;
+
+    }
+
+    public Object pop(){
+        Object element = elements[elements.length-1];
+        elements = Arrays.copyOf(elements,elements.length-1);
+        return element;
+    }
+
+    public boolean isEmpty(){
+        return elements.length == 0;
+    }
+
 }
