05596b328bbcb6f854fa91a44c719bf03ad4fb91
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-09 11:03:56 -0500

    wading

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,15 +1,21 @@
 package StackArray;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
  * Remember, you might need to resize the stack in the push method.
  * @param <E>
  */
-public class GenericStack<E> {
+public class GenericStack<E> extends Stack<E> {
     private E[] elements;
 
     public GenericStack() {
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return super.isEmpty();
     }
 }
