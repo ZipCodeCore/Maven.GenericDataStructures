dede34742b18341e6072db1095e8c6d5a1397157
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-10 18:11:46 -0500

    Finished the Table class, all tests passing

--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -10,8 +10,65 @@ import java.util.ArrayList;
  * Void return on `remove`.
  */
 public class Table<K, V> {
-    private ArrayList entries;
+    private ArrayList<Entry> entries;
     Table table;
+    public Table() {
+        entries = new ArrayList<Entry>();
+    }
+
+    public boolean contains(K key){
+        for (Entry entry:entries) {
+            if(entry.getKey().equals(key)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public V get(K key){
+        for(int i=0;i<entries.size();i++) {
+            if (entries.get(i).getKey().equals(key)) {
+                return (V) entries.get(i).getValue();
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value){
+        Entry myEntry;
+        if(contains(key)){
+              myEntry = getEntry(key);
+              int index =entries.indexOf(myEntry);
+              entries.remove(index);
+              entries.add(index,new Entry(key,value));
+        }else{
+            entries.add(new Entry(key,value));
+        }
+    }
+
+    public void remove(K key){
+        if(contains(key)){
+            for (int i=0;i<entries.size();i++) {
+                if(entries.get(i).getKey().equals(key)){
+                    entries.remove(i);
+                }
+            }
+        }
+
+    }
+    public Entry getEntry(K key){
+        for(int i=0;i<entries.size();i++) {
+            if (entries.get(i).getKey().equals(key)) {
+                return entries.get(i);
+            }
+        }
+        return null;
+
+    }
+
+
+
+
 
 
 }
