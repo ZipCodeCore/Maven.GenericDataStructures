c37800bb38fb8524f891a06fb77a65aaf34d622a
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-03-10 11:42:01 -0500

    updated stack with passing tests

--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -7,10 +7,32 @@ import java.util.ArrayList;
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
 public class Stack<E> {
-    private ArrayList elements;
+    private ArrayList<E> elements;
+    // private Object the;
 
-
-    public Stack(){
-
+    public Stack() {
+        elements = new ArrayList();
     }
+
+    // pushes the object to top of stack. works like add method but to the front instead of end
+    public void push(E item) {
+        int index = 0;
+        elements.add(index, item);
+    }
+
+    public boolean isEmpty() {
+        return elements.size() == 0;
+    }
+
+    public E pop()throws IndexOutOfBoundsException {
+       if(elements.isEmpty()) throw new IndexOutOfBoundsException("this list is empty");
+        int index = 0;
+        E removedObject = elements.get(0);
+        elements.remove(index);
+        return removedObject;
+    }
+
+
+
+
 }
