38645631fa94a68446c327ec0ff638066169dfda
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-10 16:16:31 -0500

    StackArray

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -9,7 +9,32 @@ import java.util.Arrays;
  */
 public class GenericStack<E> {
     private E[] elements;
+    private int stackSize;
+    private int topStack;
 
     public GenericStack() {
+        tempArray(10);
+    }
+    private void tempArray(int length){
+        this.stackSize = length;
+        elements = (E[]) new Object[length];
+    }
+
+    public void push(E element){
+        if (topStack <= stackSize){
+            elements[topStack] = element;
+            topStack++;
+        }
+    }
+    public E pop(){
+        E element = elements[topStack - 1];
+        topStack--;
+        return element;
+    }
+    public boolean isEmpty(){
+        if (topStack == 0){
+            return true;
+        }
+        return false;
     }
 }
