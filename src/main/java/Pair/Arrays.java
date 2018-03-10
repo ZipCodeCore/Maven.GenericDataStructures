9a3828797769c9581680980625a28e235ab31e82
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-10 16:08:53 -0500

    Pair

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -11,6 +11,20 @@ import java.util.Collections;
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
 public class Arrays {
-    public static <___> Pair<E> firstLast(ArrayList<___> a) {
+
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        E first = a.get(0);
+        E last = a.get(a.size()-1);
+        return new Pair<E>(first, last);
     }
+    public static <E extends Comparable<? super E>> E max(ArrayList<E> a){
+        return Collections.max(a);
+    }
+    public static <E extends Comparable<? super E>> E min(ArrayList<E> a){
+        return Collections.min(a);
+    }
+    public static <E extends Comparable<? super E>> Pair<E> minMax(ArrayList<E> a){
+        return new Pair<E>(Collections.min(a), Collections.max(a));
+    }
+
 }
