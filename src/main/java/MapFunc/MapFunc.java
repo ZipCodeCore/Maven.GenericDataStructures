4d0d4a8c580ad8eb418307960f57366f1e2261d3
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-11 12:57:11 -0400

    Implemented the getters and min, max methods for Pair

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -10,10 +10,10 @@ import java.util.function.Function;
 public class MapFunc {
 
 
-    public static <T,R> ArrayList map(ArrayList<T>originalArrayList, Function<T,R> functionToapply){
+    public static <T,R> ArrayList map(ArrayList<T>input, Function<T,R> functionToapply){
         ArrayList<R> mappedArrayList = new ArrayList<>();
-                 for(int i=0;i<originalArrayList.size();i++){
-                     mappedArrayList.add(functionToapply.apply(originalArrayList.get(i)));
+                 for(int i=0;i<input.size();i++){
+                     mappedArrayList.add(functionToapply.apply(input.get(i)));
                  }
             return mappedArrayList;
     }
