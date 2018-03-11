f5ea9ee1a0e28fac2a3dd8d86bbc40525d339762
CWinarski <Winarski70@yahoo.com>
2018-03-11 13:07:33 -0400

    Finished Map Function

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -7,20 +7,23 @@ import java.util.function.Function;
  * Create a function called `map` that takes an ArrayList and a `Function<T,R>` object,
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
-public class MapFunc<T> {
+public class MapFunc {
 
-    private ArrayList<T> mapArray;
-
-    public MapFunc() {
-        this.mapArray = new ArrayList<>();
-    }
-
-    public static ArrayList map(ArrayList mapArray, Function mapFunction){
+    public static <T, R> ArrayList<R> map(ArrayList<T> mapArray, Function<T, R> mapFunction) {
         ArrayList funcArray = new ArrayList();
-        for (int i = 0; i < mapArray.size(); i++ ){
-            mapFunction.apply(i);
-            funcArray.add(i);
+        for (T mapArrayValue : mapArray) {
+            R changedValue = mapFunction.apply(mapArrayValue);
+            funcArray.add(changedValue);
+
         }
-       return funcArray;
+        return funcArray;
     }
+
+    /* For this part you need to create a generic method and give it the type parameter of <T, R>
+       because the function uses T for the type being passed in. Then R for the return type.
+       The method then returns ArrayList of type R because this is the array that had the function applied to it.
+       The argument being passed in is of type T being the original array being passed into the function.
+
+     */
 }
+
--- src/test/java/MapFunc/MapFuncTest.java
+++ src/test/java/MapFunc/MapFuncTest.java
@@ -1,6 +1,5 @@
 package MapFunc;
 
-import MapFunc.MapFunc;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -14,7 +13,7 @@ public class MapFuncTest {
         intList.add(1);
         intList.add(2);
         // When it's mapped with a function to double the value
-        ArrayList<Integer> mappedList = MapFunc.map(intList, num -> num /* *2 */ );
+        ArrayList<Integer> mappedList = MapFunc.map(intList, num -> num *2);
         // Then all the values are doubled
         Assert.assertEquals(new Integer(2), mappedList.get(0));
         Assert.assertEquals(new Integer(4), mappedList.get(1));
