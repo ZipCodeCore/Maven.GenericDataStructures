a8b2ed343665d4e7967627bd3dd6c2b7fe214cd2
Carolynn Vansant <cvansant@MacBook-Pro-5.fios-router.home>
2018-03-11 19:30:40 -0400

    all 8 parts completed, putting in pull request

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -10,7 +10,38 @@ import java.util.Collections;
  * A max method that returns the largest item in the arraylist
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
-/*public class Arrays {
-    public static <___> Pair<E> firstLast(ArrayList<___> a) {
+
+public class Arrays<E> {
+
+    public static <E extends Comparable<? super E>> Pair<E> firstLast(ArrayList<E> a){
+        return new Pair<E>(a.get(0), a.get(a.size()-1));
     }
-}*/
+
+    public static <E extends Comparable<? super E>> E min(ArrayList<E> a) {
+        E min = a.get(0);
+        for (int i = 0; i < a.size(); i++) {
+            if (min.compareTo(a.get(i)) > 0) min = a.get(i);
+        }
+        return min;
+    }
+
+    public static <E extends Comparable<? super E>> E max(ArrayList<E> a) {
+        E max = a.get(0);
+        for (int i = 0; i < a.size(); i++) {
+            if (max.compareTo(a.get(i)) < 0) max = a.get(i);
+        }
+        return max;
+    }
+
+    public static <E extends Comparable<? super E>> Pair<E> minMax(ArrayList<E> a) {
+        E min = a.get(0);
+        E max = a.get(0);
+        for(int i = 0; i<a.size(); i++){
+            if(min.compareTo(a.get(i))>0) min = a.get(i);
+            if(max.compareTo(a.get(i))<0) max = a.get(i);
+        }
+        return new Pair<>(min,max);
+    }
+
+
+}
