7b2c57052d47227420e147214cb21cbb7ec87059
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-11 00:20:53 -0500

    working on ArrayListCombiner

--- src/main/java/ArrayListCombiner/ArrayListCombiner.java
+++ src/main/java/ArrayListCombiner/ArrayListCombiner.java
@@ -1,6 +1,7 @@
 package ArrayListCombiner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Create two generic methods that take two arraylists.  The methods should both append the second ArrayList's items,
@@ -8,5 +9,20 @@ import java.util.ArrayList;
  * The first method should be called extendCombiner and should use ? extends E
  * The second method should be called superCombiner and should use ? super E
  */
-public class ArrayListCombiner {
+public class ArrayListCombiner<E> {
+    private ArrayList<E> myArrayList;
+
+    public ArrayListCombiner(){
+
+    }
+
+    public static ArrayList<E> extendCombiner(ArrayList<E> first, ArrayList<? extends E>second){
+        Collections.copy(first, second);
+        return first;
+    }
+
+    public static ArrayList<? super E> superCombiner(ArrayList<E>first,ArrayList<? super E>second){
+        Collections.copy(second,first);
+        return second;
+    }
 }
