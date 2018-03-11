47cff3fec54ca72f3b34dbca0d880ea4ad564a7e
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-03-10 21:05:57 -0500

    map method is passing

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -7,6 +7,13 @@ import java.util.function.Function;
  * Create a function called `map` that takes an ArrayList and a `Function<T,R>` object,
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
-public class MapFunc {
+public class MapFunc{
+
+    public static<T,R>  ArrayList<R>  map(ArrayList<T> first, Function<T, R> second) {
+        ArrayList<R> result = new ArrayList<R>();
+        for (T object : first) result.add(second.apply(object));
+
+        return result;
+    }
 
 }
