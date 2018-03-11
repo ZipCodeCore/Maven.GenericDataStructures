9199b4e094f504b161d8ad68519b1c216aa2637a
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-03-10 19:11:31 -0500

    Part One and Two

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,5 +1,6 @@
 package StackArray;
 
+
 import java.util.Arrays;
 
 /**
@@ -9,7 +10,31 @@ import java.util.Arrays;
  */
 public class GenericStack<E> {
     private E[] elements;
+    private int top;
+    private final static int DEFAULT_SIZE = 10;
 
     public GenericStack() {
+        this(DEFAULT_SIZE);
+    }
+
+    public GenericStack(int initSize)
+    {
+        elements = (E[]) new Object [initSize];
+        top = -1;
+    }
+
+
+    public E pop() throws IndexOutOfBoundsException{
+        if (top == -1)
+            throw new IndexOutOfBoundsException();
+        return elements[top--];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public void push(E e) {
+        elements[++top] = e;
     }
 }
