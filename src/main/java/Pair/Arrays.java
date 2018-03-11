61da5d03cf3daa449abe1a0dd6c6402e1cc491b4
mbowe4 <mlbowe@gmail.com>
2018-03-11 18:14:58 -0400

    fini

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -1,16 +1,36 @@
-//package Pair;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//
-///**
-// * In here you must make firstLast, which will return a pair of the first element in the array list and the last
-// * element in the arraylist.
-// * You must also make a min method that returns the smallest item in the array list
-// * A max method that returns the largest item in the arraylist
-// * And a minmax method that returns a pair containing the largest and smallest items from the array list
-// */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+package Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * In here you must make firstLast, which will return a pair of the first element in the array list and the last
+ * element in the arraylist.
+ * You must also make a min method that returns the smallest item in the array list
+ * A max method that returns the largest item in the arraylist
+ * And a minmax method that returns a pair containing the largest and smallest items from the array list
+ */
+public class Arrays {
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        return new Pair<E>(a.get(0), a.get(a.size() -1));
+    }
+
+    public static <E extends Comparable<? super E>> E min(ArrayList<? extends E> inputArr) {
+        Collections.sort(inputArr);
+        return inputArr.get(0);
+    }
+
+    public static <E extends Comparable <? super E>> E max(ArrayList<? extends E> inputArr) {
+        Collections.sort(inputArr);
+        Collections.reverse(inputArr);
+        return inputArr.get(0);
+    }
+
+    public static <E extends  Comparable<? super E>> Pair<E> minMax(ArrayList<? extends E> input) {
+        E min = Arrays.min(input);
+        E max = Arrays.max(input);
+        return new Pair<E>(min, max);
+    }
+}
+
