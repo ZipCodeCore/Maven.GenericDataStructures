29a797b712f24a5ead3f31bfc66acd337171b4cf
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-03-10 19:42:54 -0500

    Part One, Two, Three, and Four

--- src/main/java/TableNested/TableNested.java
+++ src/main/java/TableNested/TableNested.java
@@ -8,4 +8,74 @@ import java.util.ArrayList;
  */
 public class TableNested<K, V> {
 
+    private ArrayList<Entry<K,V>> entries;
+
+
+    public TableNested() {
+        entries = new ArrayList<>();
+    }
+
+    public V get(K k) {
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getKey().equals(k)) {
+                return (V) entries.get(i).getValue();
+            }
+        }
+        return null;
+    }
+
+    public void remove(K k) {
+        if (findK(k)) {
+            for (int i = 0; i < entries.size(); i++) {
+                if (entries.get(i).getKey().equals(k)) {
+                    entries.remove(i);
+                }
+            }
+        }
+    }
+
+    public void put(K k, V v) {
+        if (findK(k)) {
+            for (int i = 0; i < entries.size(); i++) {
+                if (entries.get(i).getKey().equals(k)) {
+                    entries.remove(i);
+                    entries.add(i, new Entry(k, v));
+                }
+            }
+        } else {
+            entries.add(new Entry(k, v));
+        }
+    }
+
+
+    public boolean findK(K k) {
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getKey().equals(k)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+    class Entry<K, V> {
+
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+    }
+
