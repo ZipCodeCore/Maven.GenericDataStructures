f842ebb1fd605db58369b868e2c572cc7087ecea
ahsonali <ahsonac@gmail.com>
2018-03-12 12:59:29 -0400

    first 4 parts completed

--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -7,10 +7,39 @@ import java.util.Arrays;
  * Remember, you might need to resize the stack in the push method.
  * @param <E>
  */
-public class ObjectStack<E> {
+public class ObjectStack<E>
+{
     private Object[] elements;
 
-    public ObjectStack() {
+    public ObjectStack()
+    {
+        this.elements = new Object[0];
 
+    }
+
+    public void push(Object value)
+    {
+        Object[] newArray = (E[])Arrays.copyOf(elements, elements.length + 1);
+        newArray[newArray.length - 1] = value;
+        this.elements = newArray;
+    }
+
+    public Object pop()
+    {
+        Object value = elements[elements.length - 1];
+        Object[] newArray = Arrays.copyOf(elements, elements.length - 1);
+        this.elements = newArray;
+        return value;
+
+    }
+
+    public boolean isEmpty()
+    {
+        if(this.elements.length == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -6,11 +6,28 @@ import java.util.ArrayList;
  * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
-public class Stack<E> {
-    private ArrayList elements;
+public class Stack<E>
+{
+    private ArrayList<E> elements;
 
+    public Stack() { this.elements = new ArrayList(); }
 
-    public Stack(){
+    public boolean isEmpty()
+    {
+        return this.elements.size() == 0;
+    }
 
+    public void push(E value)
+    {
+        elements.add(value);
+    }
+
+    public E pop()
+    {
+        //Get the last element before removing it
+        E value = elements.get(elements.size() - 1);
+        elements.remove(elements.size() - 1);
+
+        return value;
     }
 }
