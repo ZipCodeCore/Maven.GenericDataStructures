b0f23b3aaee1ba22535de26c8d617e317bc75850
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-12 15:32:15 -0400

    finished whole lab

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -10,7 +10,26 @@ import java.util.Collections;
  * A max method that returns the largest item in the arraylist
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+public class Arrays {
+
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        Pair<E> pairToReturn = new Pair<E>(a.get(0), a.get(a.size() - 1));
+        return pairToReturn;
+    }
+
+    public static <E extends Comparable> E min(ArrayList<E> arrayList) {
+        Collections.sort(arrayList);
+        return arrayList.get(0);
+    }
+
+    public static <E extends Comparable> E max(ArrayList<E> arrayList) {
+        Collections.sort(arrayList);
+        return arrayList.get(arrayList.size() - 1);
+    }
+
+    public static <E extends Comparable> Pair<E> minMax(ArrayList<E> arrayList) {
+        E minimum = min(arrayList);
+        E maximum = max(arrayList);
+        return new Pair<E>(minimum, maximum);
+    }
+}
