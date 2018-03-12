15edf219d79df005fdb0f29c109e6dc0b5e2aeed
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-12 11:49:17 -0400

    finished part 2

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -11,5 +11,28 @@ public class GenericStack<E> {
     private E[] elements;
 
     public GenericStack() {
+        this.elements = (E[]) new Object[0];
+    }
+
+    public E push(E item) {
+        E[] newArray = Arrays.copyOf(this.elements, this.elements.length + 1);
+        newArray[newArray.length - 1] = item;
+        this.elements = newArray;
+        return item;
+    }
+
+    public E pop() {
+        E item = this.elements[this.elements.length - 1];
+        E[] newArray = Arrays.copyOf(this.elements, this.elements.length - 1);
+        this.elements = newArray;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        if (this.elements.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
