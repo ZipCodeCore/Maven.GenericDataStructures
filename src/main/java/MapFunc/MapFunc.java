ce668351f6575b852d6e52e71c08649d3926d5cc
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-03-12 13:37:52 -0400

    Finished lab

--- src/main/java/MapFunc/MapFunc.java
+++ src/main/java/MapFunc/MapFunc.java
@@ -2,11 +2,25 @@ package MapFunc;
 
 import java.util.ArrayList;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * Create a function called `map` that takes an ArrayList and a `Function<T,R>` object,
  * and returns an ArrayList with all of the elements of the first after the function is applied to them.
  */
 public class MapFunc {
+
+
+
+    public static <T,R> ArrayList<R> map(ArrayList<T> obj, Function<T,R> func) {
+
+        ArrayList<R> ret = new ArrayList<>();
+        for (int i = 0; i < obj.size(); i++) {
+            ret.add(func.apply(obj.get(i)));
+
+        }return ret;
+
+    }
+
 
 }
--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -10,7 +10,40 @@ import java.util.Collections;
  * A max method that returns the largest item in the arraylist
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
  */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+public class Arrays<E extends Comparable> {
+
+    public Arrays() {
+
+    }
+
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+
+        return new Pair<> (a.get(0), a.get(a.size()-1));
+
+    }
+
+
+    public static <E extends Comparable> E min(ArrayList<E> al) {
+        E min = al.get(0);
+        for (int i = 0; i < al.size(); i++)
+            if (al.get(i).compareTo(min) < 0) {
+                min = al.get(i);
+            } return min;
+
+
+    }
+
+    public static <E extends Comparable> E max(ArrayList<E> al) {
+
+        E max = al.get(0);
+        for (int i = 0; i < al.size(); i++)
+            if (al.get(i).compareTo(max) > 0) {
+                max = al.get(i);
+            } return max;
+
+    }
+
+    public static <E extends Comparable> Pair<E> minMax(ArrayList<E> al) {
+        return new Pair<E> (max(al), min(al));
+    }
+}
