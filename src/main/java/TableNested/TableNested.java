ff51faeb62b0571039edb923c204cd320c032c15
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-12 13:20:13 -0400

    finished part 3 and 4

--- src/main/java/TableNested/TableNested.java
+++ src/main/java/TableNested/TableNested.java
@@ -1,11 +1,63 @@
 package TableNested;
 
 import java.util.ArrayList;
+import java.util.Map.Entry;
+
 
 /**
  * All you need to do for this microlab is take the Table and Entry from the last one and make Entry a nested class.
  * Think about how nested classes should work with generics.
  */
 public class TableNested<K, V> {
+    private ArrayList<Entry> entries;
 
+    public TableNested() {
+        this.entries = new ArrayList<>();
+    }
+
+    public void put(K key, V value) {
+        for (Entry entry : this.entries) {
+            if (entry.getKey().equals(key)) {
+                entries.set(entries.indexOf(entry), new Entry<>(key, value));
+                return;
+            }
+        }
+        entries.add(new Entry<>(key, value));
+    }
+
+    public Object get(K key) {
+        for (Entry entry : this.entries) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        for (int i = 0; i < this.entries.size(); i++) {
+            if (this.entries.get(i).getKey().equals(key)) {
+                this.entries.remove(i);
+            }
+        }
+    }
+
+    private class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+    }
 }
