059fea1f49693f1da333092c4b704ba823ede7f9
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-03-12 12:49:55 -0400

    two more to go

--- src/main/java/ArrayListCombiner/ArrayListCombiner.java
+++ src/main/java/ArrayListCombiner/ArrayListCombiner.java
@@ -3,7 +3,8 @@ package ArrayListCombiner;
 import java.util.ArrayList;
 
 /**
- * Create two generic methods that take two arraylists.  The methods should both append the second ArrayList's items,
+ * Create two generic methods that take two arraylists.  The methods should both
+ * append the second ArrayList's items,
  * to the first.  Use a wildcard for one of the type arguments in each method.
  * The first method should be called extendCombiner and should use ? extends E
  * The second method should be called superCombiner and should use ? super E
@@ -11,5 +12,16 @@ import java.util.ArrayList;
 public class ArrayListCombiner {
 
     public ArrayListCombiner() {
+
     }
+    public static <E> ArrayList extendCombiner (ArrayList<E> first, ArrayList<? extends E> second){
+        first.addAll(second);
+        return first;
+    }
+
+    public static <E> ArrayList superCombiner (ArrayList<? super  E> first, ArrayList<E> second){
+        first.addAll(second);
+        return first;
+    }
+
 }
