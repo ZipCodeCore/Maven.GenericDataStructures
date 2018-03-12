60130a81a0227f6fe354a0b03b7ccd85aa1f913f
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-03-12 11:08:56 -0400

    part 1 & 2

--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -1,16 +1,20 @@
-package StackArray;
-
-import java.util.Arrays;
-
-/**
- * Expand the ArrayList implementation of stack here to use an Object[] array.  Still implement push, pop, and isEmpty.
- * Remember, you might need to resize the stack in the push method.
- * @param <E>
- */
-public class ObjectStack<E> {
-    private Object[] elements;
-
-    public ObjectStack() {
-
-    }
-}
+//package StackArray;
+//
+//import java.util.Arrays;
+//
+///**
+// * Expand the ArrayList implementation of stack here to use an Object[] array.  Still implement push, pop, and isEmpty.
+// * Remember, you might need to resize the stack in the push method.
+// * @param <E>
+// */
+//public class ObjectStack<E> {
+//    private Object[] elements;
+//
+//    public ObjectStack() {
+//        this.elements = new Object[];
+//    }
+//    //the way stacks work, its just like generics accept you are using Objects
+//    public Object push(Object value){
+//
+//    }
+//}
--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -7,10 +7,30 @@ import java.util.ArrayList;
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
 public class Stack<E> {
+
     private ArrayList elements;
 
 
     public Stack(){
+        this.elements = new ArrayList<E>();
+    }
 
+    public void push(E value){
+        //pushes adds an element
+        this.elements.add(value);
+    }
+
+    public E pop(){
+        //removes an element
+        return (E) elements.remove(this.elements.size()-1);
+    }
+
+    public boolean isEmpty(){
+        //Checks to see is the elements array is empty, if its equal to 0 its empty.
+        return (elements.size()==0);
+    }
+
+    public E pop() {
+        return (E) this.elements;
     }
 }
